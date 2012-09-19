fb138ea0a37b7ccbc0d9170ce89d12bbc508e692
Live session user <ubuntu@ubuntu.(none)>
2012-09-18 20:01:30 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Email::Application.config.secret_token = '97183512feb8cc84d58a980afa1a6720b3d76b869219b6042dfe1538b1fd463783810e23caf3f27d0a1a97efc4b84382c426634fa27cde4babbf7e9c6d2dedbf'
